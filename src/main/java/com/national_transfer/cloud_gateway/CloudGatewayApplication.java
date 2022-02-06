6bd8c0bfa0e082d12d09b3317a58ed068ceb5324
Houssam El aich <elaich.houssam@gmail.com>
2022-02-06 11:41:27 +0100

    Initial commit

---
+++ src/main/java/com/national_transfer/cloud_gateway/CloudGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.national_transfer.cloud_gateway;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
